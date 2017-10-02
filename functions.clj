db1204824c0fc61a4fabc0ccc9efdd730cde7704
Leandro Tk <leandrotk100@gmail.com>
2017-10-01 22:40:37 -0300

    Learning to implement function in Clojure

---
+++ functions.clj
@@ -0,0 +1,27 @@
+; Hello World function
+(defn hello-world []
+  (println "Hello World"))
+
+(hello-world "TK")
+
+; Hello Friend function with parameter
+(defn hello-friend [name]
+  (println "Hello Friend" name))
+
+(hello-friend "TK")
+
+; Sum function
+(defn sum [x y]
+  (+ x y))
+
+(sum 1 2)
+(sum 10 20)
+
+; using many parameters
+ (+ 1 2 3 4 5 6 7 8 9 10)
+
+; define function with many parameters
+(defn sum [& args]
+  (reduce + args))
+
+(sum 1 2 3 4 5 6 7 8 9 10)
